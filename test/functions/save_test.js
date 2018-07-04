ae601ea65aaa763083fb639097aacbb60a0d2c03
Sven Westerlaken <sven.westerlaken@hotmail.nl>
2018-07-04 14:07:56 +0200

    test(save): add save tests
    Make different save tests to provide more accountability on working code.

--- test/functions/save_test.js
+++ test/functions/save_test.js
@@ -1,83 +1,395 @@
-const chai = require('chai');
-const expect = chai.expect;
-chai.use(require('chai-things'));
+'use strict';
 
-const Person = require('../models/person');
-const Class = require('../models/class');
-
-const driver = require('../../lib/neo4j').getDriver();
+const { Person, Class, driver, chai, expect, int } = require('../helper');
 
 describe('Mongoose Saving', () => {
-  const test_date = new Date('31 July 1999');
+  let neil;
+  let session;
 
-  it('Should save in Mongo', (done) => {
-      const neil = new Person({
-        firstName: "Neil",
-        lastName: "Young",
-        address: {
-          city: "Brentwood",
-          street: "Barleau St."
+  beforeEach(() => {
+    neil = new Person({
+      firstName: "Neil",
+      lastName: "Young",
+      mongoSpecificValue1: "test",
+      mongoSpecificValue2: "test",
+      address: {
+        city: "Brentwood",
+        street: "Barleau St."
+      }
+    });
+
+    session = driver.session();
+  });
+
+  afterEach(() => {
+    session.close();
+  })
+
+  it('Should save a single person in Mongo & Neo4J', (done) => {
+
+    neil.save().then((result) => {
+
+      expect(result).to.not.be.null;
+      expect(result).to.include(neil);
+      expect(result).to.have.a.property('_id');
+      expect(result).to.have.a.property('mongoSpecificValue1');
+      expect(result).to.have.a.property('mongoSpecificValue2');
+      expect(result).to.have.a.property('isNew', false);
+
+      return session.run('MATCH ' +
+                         '(n:Person {m_id : {id} }) ' +
+                         'RETURN n;', {id: neil._id.toString()});
+
+    }).then((result) => {
+      const properties = result.records[0]._fields[0].properties;
+
+      expect(result).to.not.be.null;
+      expect(result.records).to.be.lengthOf(1);
+      expect(properties).to.not.have.a.property('mongoSpecificValue1');
+      expect(properties).to.not.have.a.property('mongoSpecificValue2');
+      expect(properties).to.have.a.property('m_id');
+      done();
+    });
+
+  });
+
+  it('Should save multiple persons in Mongo & Neo4J', (done) => {
+    const henry = new Person({firstName: "Henry", lastName: "McCoverty"});
+    const daniel = new Person({firstName: "Daniel", lastName: "Durval"});
+    const jason = new Person({firstName: "Jason", lastName: "Campbell"});
+
+    Person.insertMany([neil, daniel, jason, henry]).then((result) => {
+
+      expect(result).to.not.be.null,
+      expect(result).to.have.a.lengthOf(4),
+      expect(result).to.contain.something.with.a.property('firstName', neil.firstName);
+
+      return session.run('MATCH (n:Person) RETURN n;');
+
+    }).then((result) => {
+      const properties = result.records[0]._fields[0].properties;
+
+      expect(result).to.not.be.null;
+      expect(result.records).to.be.lengthOf(4);
+      expect(properties).to.have.a.property('m_id');
+      expect(properties).to.not.have.a.property('mongoSpecificValue1');
+      expect(properties).to.not.have.a.property('mongoSpecificValue2');
+      done();
+    });
+
+  });
+
+  it('Should save a subdocument as a different Node', (done) => {
+    neil.save().then((result) => {
+
+      expect(result).to.not.be.null;
+      expect(result).to.include(neil);
+      expect(result).to.have.a.property('_id');
+      expect(result).to.have.a.property('mongoSpecificValue1');
+      expect(result).to.have.a.property('mongoSpecificValue2');
+      expect(result).to.have.a.property('isNew', false);
+      expect(result).to.have.a.property('address', neil.address);
+
+      return session.run('MATCH (n:Person)-[p]-(a) RETURN n,p,a;');
+
+    }).then((result) => {
+      const results = result.records[0]._fields;
+      const properties = result.records[0]._fields[0].properties;
+
+      expect(results).to.have.a.lengthOf(3);
+      expect(results[1]).to.have.a.property('start');
+      expect(results[1]).to.have.a.property('end');
+      expect(results[1]).to.have.a.property('type');
+      expect(results[1]).to.not.have.a.property('labels');
+      expect(properties).to.have.a.property('m_id');
+      expect(properties).to.not.have.a.property('mongoSpecificValue1');
+      expect(properties).to.not.have.a.property('mongoSpecificValue2');
+      done();
+    });
+  });
+
+  it('Should save an array of subdocuments as multiple different nodes', (done) => {
+    const chemistry = new Class({
+      title: 'Chemistry',
+      books: [{
+        title: 'Organic Chemistry as a Second Language',
+        author: 'David Klein'
+      }, {
+        title: 'Chemistry: A Molecular Approach',
+        author: 'Nivaldo J. Tro'
+      }]
+    });
+
+    chemistry.save().then((result) => {
+
+      expect(result).to.not.be.null;
+      expect(result).to.have.a.property('_id');
+      expect(result).to.have.a.property('isNew', false);
+      expect(result.books).to.have.a.lengthOf(2);
+
+      return session.run('MATCH (d)-[pa]-(a:Class)-[pb]-(n) RETURN d,pa,a,pb,n;');
+
+    }).then((result) => {
+      const results = result.records[0]._fields;
+      const properties = results[0].properties;
+
+      expect(results).to.have.a.lengthOf(5);
+      expect(results[1]).to.have.a.property('start');
+      expect(results[1]).to.have.a.property('end');
+      expect(results[1]).to.have.a.property('type');
+      expect(results[1]).to.not.have.a.property('labels');
+      expect(properties).to.have.a.property('m_id');
+
+      done();
+
+    });
+  });
+
+  it('Should save a simple document reference as a relationship in Neo4J', (done) => {
+
+    neil.save().then((result) => {
+
+      expect(result).to.not.be.null;
+      expect(result).to.include(neil);
+      expect(result).to.have.a.property('_id');
+      expect(result).to.have.a.property('mongoSpecificValue1');
+      expect(result).to.have.a.property('mongoSpecificValue2');
+      expect(result).to.have.a.property('isNew', false);
+      expect(result).to.have.a.property('address', neil.address);
+
+      const chemistry = new Class({
+        title: 'Chemistry',
+        teacher: result._id
+      });
+
+      return chemistry.save();
+
+    }).then((result) => {
+
+      expect(result).to.not.be.null;
+      expect(result).to.have.a.property('_id');
+      expect(result).to.have.a.property('isNew', false);
+      expect(result).to.have.a.property('teacher');
+
+      return session.run('MATCH (n:Class)-[p]-(a:Person) RETURN n,p,a;');
+
+    }).then((result) => {
+      const results = result.records[0]._fields;
+      const properties = result.records[0]._fields[0].properties;
+
+      expect(results).to.have.a.lengthOf(3);
+      expect(results[1]).to.have.a.property('start');
+      expect(results[1]).to.have.a.property('end');
+      expect(results[1]).to.have.a.property('type');
+      expect(results[1]).to.not.have.a.property('labels');
+      expect(properties).to.have.a.property('m_id');
+      done();
+    });
+
+  });
+
+  it('Should save an array of document references as multiple relationships in Neo4J', (done) => {
+    const henry = new Person({firstName: "Henry", lastName: "McCoverty"});
+    const daniel = new Person({firstName: "Daniel", lastName: "Durval"});
+
+    Person.insertMany([daniel, henry]).then((result) => {
+
+      expect(result).to.not.be.null,
+      expect(result).to.have.a.lengthOf(2),
+      expect(result).to.contain.something.with.a.property('firstName', henry.firstName);
+      expect(result).to.contain.something.with.a.property('lastName', daniel.lastName);
+
+      const chemistry = new Class({
+        title: 'Chemistry',
+        students: [result[0]._id, result[1]._id]
+      });
+
+      return chemistry.save();
+
+    }).then((result) => {
+
+      expect(result).to.not.be.null;
+      expect(result).to.have.a.property('_id');
+      expect(result).to.have.a.property('isNew', false);
+      expect(result.students).to.be.an('Array');
+      expect(result.students).to.have.a.lengthOf(2);
+
+      return session.run('MATCH (h:Person)-[pa]-(a:Class)-[pb]-(d:Person) RETURN h,pa,a,pb,d;');
+
+    }).then((result) => {
+      const results = result.records[0]._fields;
+      const properties = results[0].properties;
+
+      expect(results).to.have.a.lengthOf(5);
+      expect(results[1]).to.have.a.property('start');
+      expect(results[1]).to.have.a.property('end');
+      expect(results[1]).to.have.a.property('type');
+      expect(results[1]).to.not.have.a.property('labels');
+      expect(properties).to.have.a.property('m_id');
+      done();
+
+    });
+
+  });
+
+  it('Should save a nested document reference as a node and a relationship with properties', (done) => {
+
+    neil.save().then((result) => {
+
+      expect(result).to.not.be.null;
+      expect(result).to.include(neil);
+      expect(result).to.have.a.property('_id');
+      expect(result).to.have.a.property('mongoSpecificValue1');
+      expect(result).to.have.a.property('mongoSpecificValue2');
+      expect(result).to.have.a.property('isNew', false);
+      expect(result).to.have.a.property('address', neil.address);
+
+      const chemistry = new Class({
+        title: 'Chemistry',
+        supervisor: {
+          person: result._id,
+          start_date: new Date('31 July 1999')
         }
       });
 
-      const henry = new Person({firstName: "Henry", lastName: "McCoverty"})
+      return chemistry.save();
 
-      const daniel = new Person({firstName: "Daniel", lastName: "Durval"});
-      const jason = new Person({firstName: "Jason", lastName: "Campbell"});
+    }).then((result) => {
 
-      Promise.all([neil.save(), Person.insertMany([daniel, jason, henry])]).then((result) => {
-        const chemistry = new Class({
-          title: 'Chemistry',
-          teacher: result[0]._id,
-          supervisor: {
-            person: result[1][2]._id,
-            start_date: test_date
-          },
-          students: [
-            result[1][0]._id,
-            result[1][1]._id
-          ],
-          books: [{
-            title: 'Organic Chemistry as a Second Language',
-            author: 'David Klein'
-          }, {
-            title: 'Chemistry: A Molecular Approach',
-            author: 'Nivaldo J. Tro'
-          }]
-        });
-        chemistry.save().then((result) => {
+      expect(result).to.not.be.null;
+      expect(result).to.have.a.property('_id');
+      expect(result).to.have.a.property('isNew', false);
+      expect(result).to.have.a.property('teacher');
 
-          const jack = new Person({
-            firstName: "Jack",
-            lastName: "Dockster",
-            takenClasses: [{
-              class: result._id,
-              grade: 6,
-              year: 2018
-            }]
-          });
-          jack.save().then(() => {
+      return session.run('MATCH (n:Class)-[p]-(a:Person) RETURN n,p,a;');
 
-            const session = driver.session();
+    }).then((result) => {
+      const results = result.records[0]._fields;
+      const properties = result.records[0]._fields[0].properties;
+      const rel_properties = result.records[0]._fields[1].properties;
 
-            session.run('MATCH (n) RETURN count(n) AS count').then((response) => {
-              let count = 0;
-              response.records.forEach((record) => count = record.get('count').low);
-              session.close();
-
-              expect(count).to.equal(9);
-
-              done();
-            });
-
-
-
-          });
-
-        });
-
-      });
+      expect(results).to.have.a.lengthOf(3);
+      expect(results[1]).to.have.a.property('start');
+      expect(results[1]).to.have.a.property('end');
+      expect(results[1]).to.have.a.property('type');
+      expect(results[1]).to.not.have.a.property('labels');
+      expect(properties).to.have.a.property('m_id');
+      expect(rel_properties).to.have.a.property('start_date');
+      done();
+    });
 
   });
+
+  it('Should save a nested array of document references as multiple nodes and relationships with properties', (done) => {
+
+    const chemistry = new Class({ title: 'Chemistry'});
+    const mathematics = new Class({ title: 'Mathematics'});
+
+    Class.insertMany([chemistry, mathematics]).then((result) => {
+
+      const jack = new Person({
+        firstName: "Jack",
+        lastName: "Landers",
+        takenClasses: [
+          {
+            class: result[0]._id,
+            grade: 6,
+            year: 2018
+          }, {
+            class: result[1]._id,
+            grade: 8,
+            year: 2017
+          }
+        ]
+      });
+
+      return jack.save();
+
+    }).then((result) => {
+
+      expect(result).to.not.be.null;
+      expect(result).to.have.a.property('_id');
+      expect(result).to.have.a.property('isNew', false);
+      expect(result.takenClasses).to.be.an('Array');
+      expect(result.takenClasses).to.have.a.lengthOf(2);
+
+      return session.run('MATCH (h:Class {title: \'Chemistry\'})-[pa]-(a:Person)-[pb]-(d:Class) RETURN h,pa,a,pb,d;');
+
+    }).then((result) => {
+      const results = result.records[0]._fields;
+      const first_properties = results[1].properties;
+      const second_properties = results[3].properties;
+
+      expect(results).to.have.a.lengthOf(5);
+      expect(results[1]).to.have.a.property('start');
+      expect(results[1]).to.have.a.property('end');
+      expect(results[1]).to.have.a.property('type');
+      expect(results[1]).to.not.have.a.property('labels');
+      expect(first_properties).to.have.properties({year: int(2018), grade: int(6)});
+      expect(second_properties).to.have.properties({year: int(2017), grade: int(8)});
+
+      done();
+
+    });
+  });
+
+  // it('Should save in Mongo', (done) => {
+  //
+  //
+  //
+  //     Promise.all([neil.save(), Person.insertMany([daniel, jason, henry])]).then((result) => {
+  //       const chemistry = new Class({
+  //         title: 'Chemistry',
+  //         teacher: result[0]._id,
+  //         supervisor: {
+  //           person: result[1][2]._id,
+  //           start_date: test_date
+  //         },
+  //         students: [
+  //           result[1][0]._id,
+  //           result[1][1]._id
+  //         ],
+  //         books: [{
+  //           title: 'Organic Chemistry as a Second Language',
+  //           author: 'David Klein'
+  //         }, {
+  //           title: 'Chemistry: A Molecular Approach',
+  //           author: 'Nivaldo J. Tro'
+  //         }]
+  //       });
+  //       chemistry.save().then((result) => {
+  //
+  //         const jack = new Person({
+  //           firstName: "Jack",
+  //           lastName: "Landers",
+  //           takenClasses: [{
+  //             class: result._id,
+  //             grade: 6,
+  //             year: 2018
+  //           }]
+  //         });
+  //         jack.save().then(() => {
+  //
+  //           const session = driver.session();
+  //
+  //           session.run('MATCH (n) RETURN count(n) AS count').then((response) => {
+  //             let count = 0;
+  //             response.records.forEach((record) => count = record.get('count').low);
+  //             session.close();
+  //
+  //             expect(count).to.equal(9);
+  //
+  //             done();
+  //           });
+  //
+  //
+  //
+  //         });
+  //
+  //       });
+  //
+  //     });
+  //
+  // });
 
 });
